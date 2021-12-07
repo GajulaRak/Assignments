be43d530a0bb9a3f31f8460c8af1ffa9c2ef68a4
GajulaRak <95267536+GajulaRak@users.noreply.github.com>
2021-12-07 15:33:47 +0530

    Add files via upload

---
+++ Exhibition.java
@@ -0,0 +1,22 @@
+package com.example;
+
+public class Exhibition extends Event {
+	int noOfStall;
+
+	public int getNoOfStall() {
+		return noOfStall;
+	}
+
+	public void setNoOfStall(int noOfStall) {
+		this.noOfStall = noOfStall;
+	}
+
+	public Exhibition(String name, String detail, String type, String ownerName, double costPerDay, int noOfStall) {
+		super(name, detail, type, ownerName, costPerDay);
+		this.noOfStall = noOfStall;
+	}
+
+	
+	
+
+}
